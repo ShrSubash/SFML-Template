39a65f32aa405288459feb9fa1e068004389954d
Subash <sbsshrestha01@gmail.com>
2022-06-29 14:53:59 +1000

    Initial Commit

---
+++ SFML Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SFML Template
+//
+//  Created by Subash shrestha on 29/6/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
